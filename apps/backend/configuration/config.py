1ecc8683b788cb1631ae5291fb14e1791d92bd12
Muhammad Hamza Khan <163638005+Muhammad-Hamza-Khan-03@users.noreply.github.com>
2026-07-08 17:46:24 +0500

    chore: add allowed origins setting

--- apps/backend/configuration/config.py
+++ apps/backend/configuration/config.py
@@ -1,17 +1,22 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
+
 class settings(BaseSettings):
-    MONGODB_URI:str
-    MONGO_DB_NAME:str
+    MONGODB_URI: str
+    MONGO_DB_NAME: str
 
-    app_name:str = "Products API"
-    environment:str = "development"
+    app_name: str = "Products API"
+    environment: str = "development"
+    allowed_origins: str = "http://localhost:3000"
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8",case_sensitive=True)
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=True,
+    )
 
 
 settings = settings()
